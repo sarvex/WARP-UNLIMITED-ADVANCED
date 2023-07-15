891a3fc48ef1d2881e037df097620ab48a26d3de
Sourcery AI <>
2023-07-15 09:41:31 +0000

    'Refactored by Sourcery'

--- warp-plus.py
+++ warp-plus.py
@@ -60,19 +60,21 @@ while True:
   if response == 200:
     SUCCESS_COUNT += 1
     log.info(f"PASSED: +1GB (total: {SUCCESS_COUNT}GB, failed: {FAIL_COUNT})")
-    if SEND_LOG and HIDE_WC_ID:
-      if not MSG_ID:
-        api_resp = httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
-        get_stats = api_resp.json()
-        try:
-          MSG_ID = get_stats["result"]["message_id"]
-        except KeyError:
-          log.error(f"API Response:\n{get_stats}")
-          raise
+    if SEND_LOG:
+      if HIDE_WC_ID:
+        if MSG_ID:
+          httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/editMessageText?chat_id={CHAT_ID}&message_id={MSG_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
+        else:
+          api_resp = httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
+          get_stats = api_resp.json()
+          try:
+            MSG_ID = get_stats["result"]["message_id"]
+          except KeyError:
+            log.error(f"API Response:\n{get_stats}")
+            raise
+      elif MSG_ID:
+        httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/editMessageText?chat_id={CHAT_ID}&message_id={MSG_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0AWARP%20ID:%20{WARP_CLIENT_ID}%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
       else:
-        httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/editMessageText?chat_id={CHAT_ID}&message_id={MSG_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
-    elif SEND_LOG and not HIDE_WC_ID:
-      if not MSG_ID:
         api_resp = httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0AWARP%20ID:%20{WARP_CLIENT_ID}%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
         get_stats = api_resp.json()
         try:
@@ -80,12 +82,10 @@ while True:
         except KeyError:
           log.error(f"API Response: {get_stats}")
           raise
-      else:
-        httpx.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/editMessageText?chat_id={CHAT_ID}&message_id={MSG_ID}&parse_mode=HTML&text=<b><u>WARP STATISTICS</u></b>%0AWARP%20ID:%20{WARP_CLIENT_ID}%0ADATA%20RECEIVED:%20%0A{SUCCESS_COUNT}GB%20%0AFAILED:%20%0A{FAIL_COUNT}")
   else:
     log.info(f"FAILED: {response}")
     FAIL_COUNT += 1
-  
+
   # Cooldown
   cooldown_time = randint(30,50)
   log.info(f"Sleep: {cooldown_time} seconds.")
